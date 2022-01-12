51c20248718d62d1a8e6de5c6e8fee0a45113d3b
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-01-12 10:50:48 -0300

    new pytest content

--- PYTEST_COURSE/restaurante.py
+++ PYTEST_COURSE/restaurante.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
+"""Classe para testagem"""
 
 class Restaurante:
-    def __init__(self, nome):
+    def __init__(self, nome, pedidos_na_fila=0):
         self.nome = nome
-        self.pedidos_na_fila == 0
+        if pedidos_na_fila >= 0:
+            self.pedidos_na_fila = pedidos_na_fila
+        else:
+            raise ValueError(
+                "A QUANTIDADE DE PEDIDOS NA FILA NÃO DEVE SER NEGATIVA"
+            )
 
+
+    def adiciona_pedido(self):
+        self.pedidos_na_fila += 1
+
+
+    def remove_pedido(self):
+        if self.pedidos_na_fila > 0:
+            self.pedidos_na_fila -= 1
--- PYTEST_COURSE/test_restaurante.py
+++ PYTEST_COURSE/test_restaurante.py
@@ -6,7 +6,6 @@ from restaurante import Restaurante
 """PRIMEIRO TESTE - teremos um restaurante que tem pedidos na fila. Esse teste deve assegurar
 que quando o restaurante iniciar com os pedidos, ele tenha 0 pedidos na fila"""
 
-
 def test_pedidos_na_fila_valor_inicial_padrao_igual_a_zero():
     restaurante = Restaurante("Pizzaria X")
     assert restaurante.pedidos_na_fila == 0
@@ -19,7 +18,6 @@ def test_pedidos_na_fila_valor_inicial_maior_que_zero():
 
 """TESTE 3 - verifica se o teste não tem numeros negativos. Porque o restaurante não
 pode ter -1 ou mais pedidos negativos."""
-
 
 def test_pedidos_na_fila_valor_inicial_menor_que_zero():
     with pytest.raises(ValueError):
@@ -36,15 +34,14 @@ def test_adiciona_um_pedido_na_fila():
 """TESTE 5 - TESTA SE O METODO remove_pedido está correto para remover, e passa que 
 o numero de pedidos deve ser igual a 0"""
 
-
 def test_remove_um_pedido_na_fila():
     restaurante = Restaurante("Pizzaria X", 1)
     restaurante.remove_pedido()
     assert restaurante.pedidos_na_fila == 0
 
+
 """TESTE 6 - verificar se o metodo remove_pedido faz com que a fila de pedidos não
 seja negativa"""
-
 
 def test_remove_um_pedido_na_fila_vazia():
     restaurante = Restaurante("Pizzaria X")
