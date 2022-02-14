966585741b833967de4f676a099267f63c283762
renatamonteiro <rcm@lionx.com.br>
2022-02-14 13:57:53 -0300

    adding more tests

--- PYTEST_EDU_MENDES/tests/test_pytest.py
+++ PYTEST_EDU_MENDES/tests/test_pytest.py
@@ -1,5 +1,6 @@
 # tests using pytest
 from src.jogo import play
+from pytest import mark
 
 """ The tests are formed for 3 steps (GWT method):
 - Given = Data
@@ -8,6 +9,7 @@ from src.jogo import play
 """
 
 
+@mark.return1
 def test_when_play_should_return_1():
     # ----- long code version
     # it should return 1
@@ -16,4 +18,33 @@ def test_when_play_should_return_1():
     result = play(entry)  # when
     assert result == expected  # then
 
-    # ----- small code version
+
+@mark.return2
+def test_when_play_should_return_2():
+    assert play(2) == 2
+
+
+@mark.returnCheese
+def test_when_play_return_cheese():
+    assert play(3) == 'CHEESE'
+
+
+@mark.returnSweetCheese
+def test_when_play_return_sweet_cheese():
+    assert play(5) == 'SWEET CHEESE'
+
+
+# using the mark skip - it will skip the test when it's not implemented yet
+@mark.skip(reason="not implemented yet")
+def test_when_play_return_1_then_none():
+    assert play(20) == 'SWEET CHEESE'
+
+
+# this test will return 5 tests, cuz I'm passing 5 numbers on entry numbers to return on the test
+@mark.parametrized
+@mark.parametrize(
+    'entry',
+    [5, 10, 20, 25, 35]
+)
+def test_play_must_return_sweet_cheese_with_5_multiples():
+    assert play(10) == 'SWEET CHEESE'
