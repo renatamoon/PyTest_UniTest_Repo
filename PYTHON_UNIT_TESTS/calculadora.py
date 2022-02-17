ce3bf2f38b64f79790de8526af7babd4d6fc1073
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-02-16 21:44:39 -0300

    adding docktest

--- PYTHON_UNIT_TESTS/calculadora.py
+++ PYTHON_UNIT_TESTS/calculadora.py
@@ -1,2 +1,17 @@
+
 def soma(x, y):
+    """"Soma x e y
+    >>> soma(-10, 20)
+    10
+    >>> soma(10, 20)
+    30
+    >>> soma('10', 20)
+    """    
+    assert isinstance(x, (int, float)), "x precisa ser int ou float"
+    assert isinstance(y, (int, float)), "y precisa ser int ou float"
     return x + y
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
--- PYTHON_UNIT_TESTS/main.py
+++ PYTHON_UNIT_TESTS/main.py
@@ -1,4 +1,7 @@
 from calculadora import soma
 
 
-print(soma(10, 20))
+try:
+    print(soma('10', 20))
+except AssertionError as e:
+    print('CONTA INVÁLIDA: {e}')
