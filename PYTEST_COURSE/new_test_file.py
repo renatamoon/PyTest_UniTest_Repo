67f69b27d7ac00f88ba36403b380b0b9dc984ab7
renatamonteiro <rcm@lionx.com.br>
2022-03-11 14:52:43 -0300

    adding tests according to the docs

---
+++ PYTEST_COURSE/new_test_file.py
@@ -0,0 +1,11 @@
+# doing these tests accordingly to the documentation
+
+# function to be tested
+def sum(x):
+    return x * 2
+
+
+# test function
+def test_if_a_number_is_equals_to_another():
+    assert function(5) == 10
+    assert function(10) != 0
