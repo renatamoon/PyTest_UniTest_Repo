f9d25edc852dfac02d9eb35e0c1582cc67553ccc
renatamonteiro <r.cardoso.monteiro1@gmail.com>
2022-03-28 17:19:10 -0300

    adding new studies

--- ASYNC_CLASS/couroutines.py
+++ ASYNC_CLASS/couroutines.py
@@ -1,6 +1,12 @@
-""" CORROTINAS - forma de escrever aplicações assincronas.
-ENTENDENDO AS CORROTINAS"""
+from collections import deque
 
+""" CORROTINAS - forma de escrever aplicações assincronas.
+ENTENDENDO AS CORROTINAS
+----------------------------------------------------------
+CONTROLE DE FLUXOS E GERADORES MELHORADOS
+"""
+
+"""
 def coro():
     print("Started")
     
@@ -31,3 +37,45 @@ print("------", next(c_value)) # None
 
 print(c_value.send([1,2,3,4,5]))
 print(next(c_value))
+"""
+
+# controlador de fluxo
+
+def count(stop):
+    cont = 1
+    
+    while cont <= stop
+        yield cont
+        cont +=1
+        
+def regressive_count(start):
+    while start >= 1:
+        yield start
+        start -= 1
+
+class Scheduler:
+    def __init__(self):
+        self.queue = deque()
+
+
+    def add_new(self, coro):
+        self.queue.append(coro)
+        
+    def run(self):
+        while self.queue:
+            task = self.queue.popleft()
+            
+            try:
+                result = next(task)
+                print(f"{task=}: {result=}")
+                self.queue.append(task)
+                
+            except StopIteration:
+                pass
+            
+            
+s = Scheduler()
+s.add_new(count(10))
+s.add_new(regressive_count(15))
+# ------------------
+
