7ba5ceb5f92f66eae6c0395c0d266b65cc7363cc
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-06-09 20:51:33 -0300

    markers xfail and xpass

--- UNITTESTING MOCKING/pytest_19/test_app.py
+++ UNITTESTING MOCKING/pytest_19/test_app.py
@@ -5,11 +5,14 @@
 Is helpful if you are expecting a test case to fail already
 - skip -
 """
+import sys
+
 import pytest
 from .app import add, multiply, divide
 
 
-@pytest.mark.xfail  # this test will be executed but it will be passed
+# you can also include reason that is a message
+@pytest.mark.xfail(reason="SHOULD BE SKIPPED")  # this test will be executed but it will be passed
 def test_add():
     assert add(10, 20) == 30
 
@@ -18,5 +21,7 @@ def test_multiply():
     assert multiply(10, 20) == 200
 
 
+# sys.version_info is the parameter that gives you the
+@pytest.mark.xfail(sys.version_info > (3, 7), reason="Python module version not compatible")
 def test_divide():
     assert divide(20, 10) == 2
